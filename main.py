b1e83992a2f05494113a954d24c061682c1812c1
abduaziz <104622399+abduazizpoytug@users.noreply.github.com>
2022-04-29 14:20:46 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,82 @@
+import time as tim
+
+def poyga():
+    print("tayyorlaning....?")
+    tim.sleep(1)
+    print(3)
+    tim.sleep(1)
+    print(2)
+    tim.sleep(1)
+    print(1)
+    tim.sleep(1)
+    print("oyin boshlandi")
+    
+
+poyga()
+
+
+
+
+
+
+
+# from multiprocessing import Event
+# import threading as the
+
+
+
+# event = the.Event()
+
+# def jarayon():
+#     print("ornatishni hohlaysizmi")
+#     event.wait()
+#     print("ornatilyapti")
+    
+# t1 = the.Thread(target=jarayon)
+# t1.start()
+
+
+# sorov = input("Dastur dastur ornatilsinmi:  ").lower()
+
+# if sorov == "ha":
+#     event.set()
+
+
+
+
+
+
+
+
+
+
+# import threading
+
+# def func():
+#     for x in range(10):
+#         print("hello")
+    
+# def func2():
+#     for x in range(10):
+#         print("hi")
+            
+# def func3():
+#     for x in range(10):
+#         print("pop")    
+        
+# def func4():
+#     for x in range(10):
+#         print("nice")
+
+
+
+
+# t1 = threading.Thread(target=func)
+# t2 = threading.Thread(target=func2)
+# t3 = threading.Thread(target=func3)
+# t4 = threading.Thread(target=func4)
+
+# t1.start()
+# t2.start()
+# t3.start()
+# t4.start()
